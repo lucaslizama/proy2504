3c178cb3c79eb64372845d2961dfdb5fc43e41bc
Lucas LizamaMonje <lucaslizama3@hotmail.com>
2017-05-02 11:26:47 -0300

    Avances

--- index.php
+++ index.php
@@ -7,6 +7,22 @@
         <link href="css/style.css" rel="stylesheet">
     </head>
     <body>
+        <?php
+            $miconn = new mysqli("10.20.26.58","root","avaras08","datospersonales");
 
+            if($miconn->connect_errno) {
+                echo "Fallo al conectar a MySQL: (" . $miconn->connect_errno . ") " . $miconn->connect_error . "\n";
+            }
+            echo $miconn->host_info . "\n<br>";
+
+            // Consultas de seleccion que devuelven un conjunto de resultados
+            if($resultado = $miconn->query("SELECT * FROM persona"))
+                echo "La seleccion deviolvio N° " . $resultado->num_rows . " filas";
+
+            // Liberar el conjunto de resultados
+            $resultado->close();
+            $miconn->close();
+        ?>
     </body>
 </html>
+
